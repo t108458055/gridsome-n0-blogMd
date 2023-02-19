54caaeaf8bcdbaf16e37adb8cf0513b0a8874bd3
t108458055 <t108458055@gmail.com>
2023-02-19 11:27:53 +0800

    html attrs modify
    change to zh

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function(Vue, { head }) {
     Vue.component('Layout', DefaultLayout)
 
-    head.htmlAttrs = { lang: 'en', class: 'h-full' }
+    head.htmlAttrs = { lang: 'zh-Hant-TW', class: 'h-full' }
     head.bodyAttrs = { class: 'antialiased font-serif' }
 
     head.link.push({
